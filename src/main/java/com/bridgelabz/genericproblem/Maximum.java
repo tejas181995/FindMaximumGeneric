2e47eda6487e4802f37b5a675071e7231fd99323
tejas181995 <tejasdev92@gmail.com>
2021-03-12 08:29:45 +0530

    Refactored with generic class

--- src/main/java/com/bridgelabz/genericproblem/Maximum.java
+++ src/main/java/com/bridgelabz/genericproblem/Maximum.java
@@ -1,6 +1,18 @@
 package com.bridgelabz.genericproblem;
 
-public class Maximum {
+public class Maximum <E extends Comparable<E>>{
+    E value1, value2, value3;
+
+    public Maximum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public E maximumFind() {
+        return Maximum.findMaximum(value1, value2, value3);
+    }
+
     public static <E extends Comparable<E>> E findMaximum(E value1, E value2, E value3) {
         E max = value1;
         if (value2.compareTo(max) > 0) {
--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -4,22 +4,26 @@ import org.junit.jupiter.api.Test;
 import com.bridgelabz.genericproblem.Maximum;
 
 
-public class TestMaximum {
+public class TestMaximum <E extends Comparable<E>> {
+    Maximum maximum;
     @Test
     public void givenThreeIntegers_ShouldReturnMaximumValue() {
-        Integer maxInteger = Maximum.findMaximum(7, 3, 5);
+        maximum = new Maximum<>(7, 3, 5);
+        Integer maxInteger = (Integer) maximum.maximumFind();
         Assert.assertEquals(Integer.valueOf(7), maxInteger);
     }
 
     @Test
     public void givenThreeFloats_ShouldReturnMaximumValue() {
-        Float maxFloat = Maximum.findMaximum(7.4f, 8.9f, 5.5f);
+        maximum = new Maximum<>(7.4f, 8.9f, 5.5f);
+        Float maxFloat = (Float) maximum.maximumFind();
         Assert.assertEquals(Float.valueOf(8.9f), maxFloat);
     }
 
     @Test
     public void givenThreeString_ShouldReturnMaximumValue() {
-        String maxString = Maximum.findMaximum("Papaya", "Apple", "WaterMelon");
+        maximum = new Maximum<>("Papaya", "Apple", "WaterMelon");
+        String maxString = (String) maximum.maximumFind();
         Assert.assertEquals("WaterMelon", maxString);
     }
 }
