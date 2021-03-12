6db071143bf848589413ad733d9aabfe5fd36fd7
tejas181995 <tejasdev92@gmail.com>
2021-03-12 19:32:26 +0530

    Refactored findMaximum method

--- src/main/java/com/bridgelabz/genericproblem/Maximum.java
+++ src/main/java/com/bridgelabz/genericproblem/Maximum.java
@@ -1,25 +1,28 @@
 package com.bridgelabz.genericproblem;
 
 public class Maximum {
-    public static <E extends Comparable<E>> E findMaximum(E... intNum) {
-        for ( int i = 0; i < intNum.length; i ++ ) {
-            System.out.println(intNum[i]);
+    public static  <E extends Comparable<E>> E findMaximum(E... intNum) {
+        for (E e : intNum) {
+            System.out.println(e);
         }
+
         E temp;
-        for (int i =0 ; i<(intNum.length - 1 ); i ++) {
-            for ( int j = 0; j<(intNum.length - 1); j++) {
-                if ( intNum[j].compareTo(intNum[j + 1]) < 0 ) {
-                    temp = intNum[j] ;
-                    intNum[j] = intNum[j + 1] ;
-                    intNum[j+1] = temp ;
+
+        for (int i = 0; i < (intNum.length - 1); i++) {
+            for (int j = 0; j < (intNum.length - 1); j++) {
+                if (intNum[j].compareTo(intNum[j + 1]) < 0) {
+                    temp = intNum[j];
+                    intNum[j] = intNum[j + 1];
+                    intNum[j + 1] = temp;
                 }
             }
         }
-        E max = intNum[0];
-        return max;
+
+        return intNum[0];
     }
-    public static <E> E printMax(E max) {
-        System.out.println("Maximum Value is:" + max) ;
-        return max;
+
+    public static <E> void printMax(E max) {
+
+        System.out.println("Maximum Value is:" + max);
     }
 }
--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -1,32 +1,36 @@
+import com.bridgelabz.genericproblem.Maximum;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.bridgelabz.genericproblem.Maximum;
 
 
-public class TestMaximum <E extends Comparable<E>> {
+public class TestMaximum<E extends Comparable<E>, findMaximum> {
     Maximum maximum;
     @Test
     public void givenThreeIntegerNumber_ShouldReturnMaximumValue() {
-        Integer[] intNum = { 6, 2, 1, 11, 20 } ;
+        Integer[] intNum = {6, 2, 1, 11, 20};
+       // maximum = new findMaximum();
         Integer maxInteger = maximum.findMaximum(intNum);
-        maximum.printMax(maxInteger) ;
-        Assert.assertEquals( Integer.valueOf(20),  maxInteger);
+        maximum.printMax(maxInteger);
+        Assert.assertEquals(Integer.valueOf(20), maxInteger);
     }
 
     @Test
     public void givenThreeFloatNumber_ShouldReturnMaximumValue() {
-        Float[] floatNum = { 6.1f, 2.8f, 4.6f, 1.1f, 9.8f } ;
+        Float[] floatNum = {6.1f, 2.8f, 4.6f, 1.1f, 9.8f};
+        //maximum = new findMaximum();
         Float maxFloat = maximum.findMaximum(floatNum);
-        maximum.printMax(maxFloat) ;
-        Assert.assertEquals( Float.valueOf(9.8f),  maxFloat);
+        maximum.printMax(maxFloat);
+        Assert.assertEquals(Float.valueOf(9.8f), maxFloat);
     }
 
     @Test
     public void givenThreeStrings_ShouldReturnMaximumString() {
-        String[] string = { " apple", " watermelon ", " orange " } ;
+        String[] string = {" apple", " watermelon ", " orange "};
+       // maximum = new findMaximum();
         String maxString = maximum.findMaximum(string);
-        maximum.printMax(maxString) ;
-        Assert.assertEquals( " watermelon ",  maxString);
+        maximum.printMax(maxString);
+        Assert.assertEquals(" watermelon ", maxString);
     }
 }
